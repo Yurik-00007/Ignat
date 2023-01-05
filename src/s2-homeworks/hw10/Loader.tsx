c08ba52de48bcf4c77cd46a8517fd2f564669df9
Timofeev Yury <yurik-007@mail.ru>
2023-01-05 19:33:46 +0300

    05.01.23_10_1

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,6 @@
 import s from './Loader.module.css'
+import preloader from './Spinner-1s-200px.svg';
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    <img src={preloader}/>
+</div>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,9 +2,11 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof initState=> { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING':
+            return {...state,isLoading:action.isLoading}
 
         default:
             return state
@@ -15,7 +17,7 @@ type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
 }
-
+//type LoadingACType=ReturnType<typeof loadingAC>
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
